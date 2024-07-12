9fe5d063ae31ea5e45c10b9557139c797424be25
Michflorez <167869106+Michflorez@users.noreply.github.com>
2024-07-12 19:33:41 +0000

    trafficLight Mich-florez

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,8 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Home } from './component/home';
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
 
+ReactDOM.render(< Home />, document.getElementById('app'));
